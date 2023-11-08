a9be1da4ac304a72084082a50b33e68667541522
youayou <youayou121@github.com>
2023-11-08 23:13:04 +0800

    update

--- scripts/servo_commands.py
+++ scripts/servo_commands.py
@@ -13,8 +13,8 @@ def set_throttle_steer(data):
 
     pub_vel_left_rear_wheel = rospy.Publisher('/racecar/rear_left_velocity_controller/command', Float64, queue_size=1)
     pub_vel_right_rear_wheel = rospy.Publisher('/racecar/rear_right_velocity_controller/command', Float64, queue_size=1)
-    # pub_vel_left_front_wheel = rospy.Publisher('/racecar/left_front_wheel_velocity_controller/command', Float64, queue_size=1)
-    # pub_vel_right_front_wheel = rospy.Publisher('/racecar/right_front_wheel_velocity_controller/command', Float64, queue_size=1)
+    pub_vel_left_front_wheel = rospy.Publisher('/racecar/front_left_velocity_controller/command', Float64, queue_size=1)
+    pub_vel_right_front_wheel = rospy.Publisher('/racecar/front_right_velocity_controller/command', Float64, queue_size=1)
 
     pub_pos_left_steering_hinge = rospy.Publisher('/racecar/front_left_steering_position_controller/command', Float64, queue_size=1)
     pub_pos_right_steering_hinge = rospy.Publisher('/racecar/front_right_steering_position_controller/command', Float64, queue_size=1)
@@ -24,8 +24,8 @@ def set_throttle_steer(data):
 
     pub_vel_left_rear_wheel.publish(throttle)
     pub_vel_right_rear_wheel.publish(throttle)
-    # pub_vel_left_front_wheel.publish(throttle)
-    # pub_vel_right_front_wheel.publish(throttle)
+    pub_vel_left_front_wheel.publish(throttle)
+    pub_vel_right_front_wheel.publish(throttle)
     pub_pos_left_steering_hinge.publish(steer)
     pub_pos_right_steering_hinge.publish(steer)
 
